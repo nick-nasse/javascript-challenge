eed3cc241b501321e15dc69ceefcec79a8cf3c7d
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-09-23 00:02:31 -0700

    dynamic table created

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,18 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Console.log the data from data.js
+// console.log(data);
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+
+// create dynamic html table
+data.forEach((Ufosighting) => {
+    var row = tbody.append("tr");
+    Object.entries(Ufosighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
