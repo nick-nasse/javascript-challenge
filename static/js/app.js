3e7def2447b094675e41bdedc0f959de228c0ccd
Nick Nasse <nasse@Nasses-Pro-Book.local>
2020-09-23 08:19:02 -0700

    reworked the table function and completed the rest of challenge

--- static/js/app.js
+++ static/js/app.js
@@ -9,10 +9,37 @@ var tbody = d3.select("tbody");
 
 
 // create dynamic html table
-data.forEach((Ufosighting) => {
-    var row = tbody.append("tr");
-    Object.entries(Ufosighting).forEach(([key, value]) => {
-      var cell = row.append("td");
-      cell.text(value);
+// Build Table Function.
+function tabular(generic) {
+    tbody.html("");
+    // Use d3 to update each cell's text with UFO Sigthings Data.
+    generic.forEach(UFOSightings => {
+        let row = tbody.append("tr");
+        Object.values(UFOSightings).forEach((value) => {
+            let cell = row.append("td");
+            cell.text(value);
+        });
     });
-  });
+};
+
+console.log(tableData)
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    dateInput = d3.select("#datetime").property("value");
+
+    var filterData = tableData;
+    var filterData = filterData.filter((row) => row.datetime === dateInput);
+
+    tabular(filterData);
+    console.log(filterData);
+};
+
+d3.selectAll("#filter-btn").on("click", runEnter);
+d3.selectAll("#form").on("submit", runEnter);
+tabular(tableData);
